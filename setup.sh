deb3b6666291e4de86fde643c56c9f787cd87c1e
monash-brandon-wee <126179133+monash-brandon-wee@users.noreply.github.com>
2025-02-25 23:08:35 +0800

    Changes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
+python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('wordnet')"
 
 # Run database migrations (if using Flask-Migrate)
 # flask db upgrade
